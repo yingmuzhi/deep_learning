70a4ed26c892a289ef528de1d27d06d752b63b0d
yingmuzhi <cyxscj@126.com>
2023-07-05 07:38:09 +0000

    update

---
+++ chapter9/9.2. Converting Raw Text into Sequence Data.py
@@ -0,0 +1,112 @@
+
+import collections
+import random
+import re
+import torch
+from d2l import torch as d2l
+
+
+"""
+
+download dataset
+"""
+class TimeMachine(d2l.DataModule): #@save
+    """The Time Machine dataset."""
+    def _download(self):
+        fname = d2l.download(d2l.DATA_URL + 'timemachine.txt', self.root,
+                             '090b5e7e70c295757f55df93cb0a180b9691891a')
+        with open(fname) as f:
+            return f.read()
+
+data = TimeMachine()
+raw_text = data._download()
+print(raw_text[:60])
+
+
+
+
+"""
+
+preprocess
+忽略标点和大小写
+将文本行列表lines转换为tokens词元
+"""
+# 忽略标点和大小写
+@d2l.add_to_class(TimeMachine)  #@save
+def _preprocess(self, text):
+    return re.sub('[^A-Za-z]+', ' ', text).lower()
+
+text = data._preprocess(raw_text)
+print(text[:60])
+
+
+"""
+
+tokenization
+词元化
+"""
+@d2l.add_to_class(TimeMachine)  #@save
+def _tokenize(self, text):
+    return list(text)
+
+tokens = data._tokenize(text)
+print(','.join(tokens[:30]))
+
+
+"""
+
+vocabulary
+将 token词元 按照 vocabulary词表 映射到 number
+"""
+class Vocab:  #@save
+    """Vocabulary for text."""
+    def __init__(self, tokens=[], min_freq=0, reserved_tokens=[]):
+        # Flatten a 2D list if needed
+        if tokens and isinstance(tokens[0], list):
+            tokens = [token for line in tokens for token in line]
+        # Count token frequencies
+        counter = collections.Counter(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1],
+                                  reverse=True)
+        # The list of unique tokens
+        self.idx_to_token = list(sorted(set(['<unk>'] + reserved_tokens + [
+            token for token, freq in self.token_freqs if freq >= min_freq])))
+        self.token_to_idx = {token: idx
+                             for idx, token in enumerate(self.idx_to_token)}
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if hasattr(indices, '__len__') and len(indices) > 1:
+            return [self.idx_to_token[int(index)] for index in indices]
+        return self.idx_to_token[indices]
+
+    @property
+    def unk(self):  # Index for the unknown token
+        return self.token_to_idx['<unk>']
+    
+vocab = Vocab(tokens)
+indices = vocab[tokens[:10]]
+print('indices:', indices)
+print('words:', vocab.to_tokens(indices))
+
+
+"""
+
+together
+"""
+@d2l.add_to_class(TimeMachine)  #@save
+def build(self, raw_text, vocab=None):
+    tokens = self._tokenize(self._preprocess(raw_text))
+    if vocab is None: vocab = Vocab(tokens)
+    corpus = [vocab[token] for token in tokens]
+    return corpus, vocab
+
+corpus, vocab = data.build(raw_text)
+len(corpus), len(vocab)
